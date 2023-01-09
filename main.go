3ea8387637f1dee4e97bf017a756c541b3c192ef
andrew <PositiveWay05@gmail.com>
2023-01-09 19:42:26 +0500

    send as raw bytes

--- main.go
+++ main.go
@@ -8,29 +8,31 @@ import (
 
 func toNum(bytes []byte) int {
 	num := int(bytes[1])
-	switch rune(bytes[0]) {
-	case '+':
+	switch bytes[0] {
+	case 0:
 		return num
-	case '-':
+	case 1:
 		return -num
 	default:
-		panic(fmt.Sprintf("unsupported value: %s", string(bytes)))
+		panic(fmt.Sprintf("unsupported value: %v", bytes[0]))
 	}
 }
 
 func control(event []byte) {
 	//fmt.Println(event)
 
-	commandType := rune(event[0])
-	command := string(event[1:])
-
-	switch commandType {
-	case '+', '-':
+	if event[0] == 0 || event[0] == 1 {
 		if len(event) != 4 {
 			gofuncs.Panic("Incorrect length: %v", len(event))
 		}
 		x, y := toNum(event[0:2]), toNum(event[2:4])
 		osSpec.MoveMouse(x, y)
+		return
+	}
+
+	commandType := rune(event[0])
+	command := string(event[1:])
+	switch commandType {
 	case 'p':
 		//gofuncs.Print("press")
 		osSpec.PressKeyOrMouse(command)
