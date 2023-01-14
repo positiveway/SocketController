8f1c9b731b088c4d0fdd5053a47266f188e045ef
andrew <PositiveWay05@gmail.com>
2023-01-14 17:04:24 +0500

    back to udp

--- main.go
+++ main.go
@@ -32,11 +32,6 @@ func main() {
 	const RightMouse = 91
 	const MiddleMouse = 92
 
-	addr := net.TCPAddr{
-		Port: 5005,
-		IP:   net.ParseIP("0.0.0.0"),
-	}
-
 	keyboard, err := uinput.CreateKeyboard("/dev/uinput", []byte("testkeyboard"))
 	gofuncs.CheckErr(err)
 	defer keyboard.Close()
@@ -44,7 +39,12 @@ func main() {
 	gofuncs.CheckErr(err)
 	defer mouse.Close()
 
-	server, err := net.ListenTCP("tcp4", &addr)
+	addr := net.UDPAddr{
+		Port: 5005,
+		IP:   net.ParseIP("0.0.0.0"),
+	}
+
+	server, err := net.ListenUDP("udp4", &addr)
 	if err != nil {
 		panic(fmt.Sprintf("Client is already running: %v", err))
 	}
@@ -57,64 +57,54 @@ func main() {
 	runtime.GC()
 
 	for {
-		func() {
-			conn, err := server.AcceptTCP()
-			if err != nil {
-				gofuncs.Panic("Connection err: %v", err)
-			}
-			defer conn.Close()
+		msgLen, _, err := server.ReadFrom(msg)
+		if err != nil {
+			gofuncs.Print("Client disconnected. Read err: %v", err)
+			continue
+		}
+		//fmt.Printf("%v %v\n", int(msg[0]), int(msg[1]))
 
-			for {
-				msgLen, err := conn.Read(msg)
-				if err != nil {
-					gofuncs.Print("Client disconnected. Read err: %v", err)
-					return
-				}
-				//fmt.Printf("%v %v\n", int(msg[0]), int(msg[1]))
-
-				if msgLen == 2 {
-					if msg[0] == 128 {
-						y := toNum(msg[1])
-						y = getSign(y)
-						mouse.Wheel(false, y)
-					} else if msg[1] == 128 {
-						x := toNum(msg[0])
-						x = getSign(x)
-						mouse.Wheel(true, x)
-					} else {
-						x := toNum(msg[0])
-						y := toNum(msg[1])
-						//fmt.Printf("%v %v\n", x, y)
-						mouse.Move(x, -y)
-					}
-				} else if msgLen == 1 {
-					if msg[0] > 128 {
-						msg[0] -= 128
-						switch msg[0] {
-						case LeftMouse:
-							mouse.LeftPress()
-						case RightMouse:
-							mouse.RightPress()
-						case MiddleMouse:
-							mouse.MiddlePress()
-						default:
-							keyboard.KeyDown(int(msg[0]))
-						}
-					} else {
-						switch msg[0] {
-						case LeftMouse:
-							mouse.LeftRelease()
-						case RightMouse:
-							mouse.RightRelease()
-						case MiddleMouse:
-							mouse.MiddleRelease()
-						default:
-							keyboard.KeyUp(int(msg[0]))
-						}
-						runtime.GC()
-					}
-				}
+		if msgLen == 2 {
+			if msg[0] == 128 {
+				y := toNum(msg[1])
+				y = getSign(y)
+				mouse.Wheel(false, y)
+			} else if msg[1] == 128 {
+				x := toNum(msg[0])
+				x = getSign(x)
+				mouse.Wheel(true, x)
+			} else {
+				x := toNum(msg[0])
+				y := toNum(msg[1])
+				//fmt.Printf("%v %v\n", x, y)
+				mouse.Move(x, -y)
 			}
-		}()
+		} else if msgLen == 1 {
+			if msg[0] > 128 {
+				msg[0] -= 128
+				switch msg[0] {
+				case LeftMouse:
+					mouse.LeftPress()
+				case RightMouse:
+					mouse.RightPress()
+				case MiddleMouse:
+					mouse.MiddlePress()
+				default:
+					keyboard.KeyDown(int(msg[0]))
+				}
+			} else {
+				switch msg[0] {
+				case LeftMouse:
+					mouse.LeftRelease()
+				case RightMouse:
+					mouse.RightRelease()
+				case MiddleMouse:
+					mouse.MiddleRelease()
+				default:
+					keyboard.KeyUp(int(msg[0]))
+				}
+				runtime.GC()
+			}
+		}
 	}
 }
